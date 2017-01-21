143f787f9ab22affcfef2711f91c200d0cc1860e
Nvertigo <nvertigo67@gmail.com>
2017-01-21 19:42:18 +0100

    msm8996.mk: set first screen size to large.

--- msm8996.mk
+++ msm8996.mk
@@ -122,7 +122,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     camera.disable_zsl_mode=1
 
 # List of AAPT configurations
-PRODUCT_AAPT_CONFIG += xlarge large
+PRODUCT_AAPT_CONFIG += large xlarge
 
 #for android_filesystem_config.h
 PRODUCT_PACKAGES += \
